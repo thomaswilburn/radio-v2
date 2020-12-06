b4d8b0670931d98621214b412475b495f59b2901
Thomas Wilburn <tbone@thomaswilburn.net>
2020-12-06 18:44:54 +0000

    Add share target support

--- service-worker.js
+++ service-worker.js
@@ -8,26 +8,5 @@ addEventListener("activate", function(e) {
 });
 
 addEventListener("fetch", async function(e) {
-  var url = new URL(e.request.url);
-  // short-circuit external (or proxied) requests
-  if (url.hostname != location.hostname || url.pathname.match(/^\/proxy/)) return;
 
-  console.log(`Intercepted fetch for ${url}`);
-  var respond = async function() {
-    var cache = await caches.open("offline");
-    try {
-      var network = await fetch(e.request);
-      if (network.status >= 300) throw "Redirected for 'local' files";
-      e.waitUntil(cache.put(e.request, network.clone()));
-      console.log(`Cached ${url}`);
-      return network;
-    } catch (err) {
-      console.log(err);
-      console.log(`Network failed, returning cache for ${url}`);
-      return cache.match(e.request);
-    }
-  };
-
-  e.respondWith(respond());
-  
 });
